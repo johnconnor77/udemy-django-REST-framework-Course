14e077d296d433fb1d550984be1532cfaaee33fd
johnconnor77 <jfcalleh@unal.edu.co>
2022-05-20 00:38:15 -0500

    Added Method Field for checking the lenght of the name

--- imdbclone/watchlist_app/api/seriallizers.py
+++ imdbclone/watchlist_app/api/seriallizers.py
@@ -2,31 +2,43 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
-class MovieSerializers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializers(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # fields = ['id', 'name', 'description']
+        # exclude = ['active']
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+    def get_len_name(self, object):
+        return  len(object.name)
 
-    def validate(self, data):
-        if data['name'] ==  data['description']:
-            raise serializers.ValidationError("Title and Description should be different!")
-        else:
-            return data
-
-    def validate_name(self, value):
-
-        if len(value) < 2:
-            raise serializers.ValidationError("Name is too short")
-        else:
-            return value
+# class MovieSerializers(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     def validate(self, data):
+#         if data['name'] ==  data['description']:
+#             raise serializers.ValidationError("Title and Description should be different!")
+#         else:
+#             return data
+#
+#     def validate_name(self, value):
+#
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")
+#         else:
+#             return value
