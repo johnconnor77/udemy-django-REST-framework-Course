caf58e8d5dd18e8b55979d20dbe69d7c6494d2f3
johnconnor77 <jfcalleh@unal.edu.co>
2022-05-21 03:05:35 -0500

    Added functionalities for supporting Review model

--- imdbclone_v2/watchlist_app/api/urls.py
+++ imdbclone_v2/watchlist_app/api/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
 
 
-urlpatterns = [
+urlpatterns = {
     path('list/', WatchListAV.as_view(), name='watch_list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='watch_list_details'),
-    path('stream/', StreamPlatformAV.as_view() , name='stream_plaforms'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream_plaforms'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_platform_details'),
-]
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    #path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='stream_platform_details'),
+    #path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
+}
